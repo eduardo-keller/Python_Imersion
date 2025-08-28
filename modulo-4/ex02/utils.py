16d3c5d063c68321a467118c02c3ed23a8d22894
eduardo <edkeller@student.42sp.org.br>
2025-08-28 18:59:51 -0300

    waka test

--- modulo-4/ex02/utils.py
+++ modulo-4/ex02/utils.py
@@ -1,3 +1,10 @@
 
-def format_cents(value: int) -> str:
+def format_cents(cents: int) -> str:
+    if cents < 0:
+        sign: str = "[-]"
+        cents = cents * -1
+    else:
+        sign: str = "[+]"
+    cents = cents / 100
     
+
